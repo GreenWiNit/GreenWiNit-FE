3fa47384848718ff695e56a50c06e929aceb2a39
sacru2red <sacru2red@gmail.com>
2025-08-26 00:01:41 +0900

    chore: migrate pages to worker

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 import { defineConfig, loadEnv } from 'vite'
 import svgr from 'vite-plugin-svgr'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 // https://vite.dev/config/
 export default defineConfig(({ mode }) => {
@@ -19,6 +20,7 @@ export default defineConfig(({ mode }) => {
       react(),
       svgr(),
       tailwindcss(),
+      cloudflare(),
     ],
     resolve: {
       alias: {
