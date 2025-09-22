37a58178e39dbbf60d3ba219758b56f1bfcba7b5
beomjo <dongjun1315@naver.com>
2025-09-22 17:19:31 +0900

    fix: add home challenge preview skeleton ui

--- src/components/home-screen/challenges/index.tsx
+++ src/components/home-screen/challenges/index.tsx
@@ -9,17 +9,22 @@ import { Carousel, CarouselContent, CarouselItem } from '@/components/shadcn/car
 import { cn } from '@/lib/utils'
 import SuggestChallenge from './suggest_challenge'
 import { ChevronRight } from 'lucide-react'
+import { Button } from '@/components/shadcn/button'
 
 export const ChallengePreview = ({ category, challenges }: ChallengePreviewProps) => {
   const navigate = useNavigate()
   const isLoggedIn = useIsLoggedIn()
   const [isWarnNotLoggedInDialogOpen, setIsWarnNotLoggedInDialogOpen] = useState(false)
 
-  const handleViewButtonClick = (category: 'individual' | 'team') => {
+  const handleViewButton = (category: 'individual' | 'team') => {
     navigate({
       to: `/challenges`,
       search: { tab: category },
     })
+  }
+
+  const handleMakeChallenge = () => {
+    navigate({ to: '/challenges/create' })
   }
 
   return (
@@ -30,32 +35,44 @@ export const ChallengePreview = ({ category, challenges }: ChallengePreviewProps
         </h3>
         <div
           className="text-notify-gray flex cursor-pointer flex-row items-center pr-0 text-sm"
-          onClick={() => handleViewButtonClick(category)}
+          onClick={() => handleViewButton(category)}
         >
           <span>전체보기</span>
           <ChevronRight color="gray" size={20} />
         </div>
       </div>
       <Carousel className="mt-4">
-        <CarouselContent className={cn('-ml-4', 'max-w-[200px]')}>
-          {challenges?.map((challenge) => (
-            <CarouselItem key={challenge.id} className="max-w-[200px] pb-1 pl-4">
-              <Challenge
-                challenge={challenge}
-                onClick={() => {
-                  if (!isLoggedIn) {
-                    setIsWarnNotLoggedInDialogOpen(true)
-                    return
-                  }
-                  navigate({
-                    to: `/challenges/${challenge.id}/detail`,
-                    search: { challengeType: category },
-                  })
-                }}
-              />
-            </CarouselItem>
-          ))}
-        </CarouselContent>
+        {challenges?.length === 0 ? (
+          <div className="m-4 flex h-30 w-full flex-col items-center justify-center gap-4">
+            <p className="text-ring whitespace-pre-line">{`만들어진 챌린지가 아직 없어요.\n챌린지를 만들어 보세요!`}</p>
+            <Button
+              className="bg-mountain_meadow-500 font-bold text-white"
+              onClick={handleMakeChallenge}
+            >
+              챌린지 만들러 가기
+            </Button>
+          </div>
+        ) : (
+          <CarouselContent className={cn('-ml-4', 'max-w-[200px]')}>
+            {challenges?.map((challenge) => (
+              <CarouselItem key={challenge.id} className="max-w-[200px] pb-1 pl-4">
+                <Challenge
+                  challenge={challenge}
+                  onClick={() => {
+                    if (!isLoggedIn) {
+                      setIsWarnNotLoggedInDialogOpen(true)
+                      return
+                    }
+                    navigate({
+                      to: `/challenges/${challenge.id}/detail`,
+                      search: { challengeType: category },
+                    })
+                  }}
+                />
+              </CarouselItem>
+            ))}
+          </CarouselContent>
+        )}
       </Carousel>
       <WarnNotLoggedIn
         isOpen={isWarnNotLoggedInDialogOpen}
