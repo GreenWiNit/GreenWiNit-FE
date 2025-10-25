8552d8c23a948ce56c0281a8be41a91e87055478
taewok <taewok5163@naver.com>
2025-10-25 15:53:03 +0900

    feat: 꾸미기 아이템 드래그, 배치 기능

---
+++ src/components/dashboard/decoration-item.tsx
@@ -0,0 +1,45 @@
+import { memo, useRef } from 'react'
+import Draggable, { DraggableData } from 'react-draggable'
+
+interface DecorationItemProps {
+  Img: React.FC<React.SVGProps<SVGSVGElement>>
+  position: { x: number; y: number }
+  /**  */
+  handlePlacedItemList: (x: number, y: number) => void
+  handleDragStart: () => void
+  index: number
+  isActive: boolean
+}
+
+const DecorationItem = ({
+  Img,
+  position,
+  handlePlacedItemList,
+  handleDragStart,
+  index,
+  isActive,
+}: DecorationItemProps) => {
+  const nodeRef = useRef<HTMLDivElement>(null)
+  return (
+    <Draggable
+      nodeRef={nodeRef}
+      bounds="parent"
+      defaultPosition={position}
+      enableUserSelectHack={false} //드래그 시 텍스트 선택 방지 해제
+      onStop={(_, data: DraggableData) => {
+        handlePlacedItemList(data.x, data.y)
+      }}
+      onStart={handleDragStart}
+    >
+      <div
+        ref={nodeRef}
+        className="absolute cursor-grab"
+        style={{ zIndex: isActive ? 11 : index + 1 }}
+      >
+        <Img width={80} height={80} />
+      </div>
+    </Draggable>
+  )
+}
+
+export default memo(DecorationItem)
--- src/routes/dashboard.tsx
+++ src/routes/dashboard.tsx
@@ -12,7 +12,11 @@ import Lv2Img from '@/components/dashboard/lv2.svg?react'
 import Lv3Img from '@/components/dashboard/lv3.svg?react'
 import Lv4Img from '@/components/dashboard/lv4.svg?react'
 import MyItemModal from '@/components/dashboard/my-item-modal'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
+import DecorationItem from '@/components/dashboard/decoration-item'
+import { Item, PlacedItem } from '@/types/dashboard'
+import { levelImgs } from '@/constant/dashboard-level-image'
+import { v4 as uuid } from 'uuid'
 
 export const Route = createFileRoute('/dashboard')({
   component: RouteComponent,
@@ -25,6 +29,7 @@ function RouteComponent() {
     setIsItemModal((prev) => !prev)
   }
 
+  //현재 레벨
   const currentLevel = 1
 
   //현재 레벨의 진행 수치
@@ -40,8 +45,97 @@ function RouteComponent() {
     { id: 4, Component: DiskIcon, onClick: toggleItemModal },
   ]
 
+  //나의 아이템 리스트
+  const [itemList, setItemList] = useState<Item[]>([
+    { id: 546, name: '아이템1', count: 2, img: Lv1Img },
+    { id: 847, name: '아이템2', count: 2, img: Lv2Img },
+    { id: 123, name: '아이템3', count: 1, img: Lv3Img },
+    { id: 111, name: '아이템4', count: 1, img: Lv4Img },
+    { id: 334, name: '아이템5', count: 1, img: Lv4Img },
+    { id: 777, name: '아이템6', count: 1, img: Lv4Img },
+  ])
+
+  //대시보드 꾸미기 아이템 컨테이너 ref
+  const decorationContainerRef = useRef<HTMLDivElement>(null)
+  //대시보드 꾸미기 아이템 컨테이너 크기
+  const [decorationContainerSize, setDecorationContainerSize] = useState<{
+    width: number
+    height: number
+  }>({ width: 0, height: 0 })
+
+  //대시보드 꾸미기 아이템 컨테이너 값 구하기
+  useEffect(() => {
+    if (decorationContainerRef.current) {
+      const { width, height } = decorationContainerRef.current.getBoundingClientRect()
+      setDecorationContainerSize({ width, height })
+    }
+  }, [])
+
+  /** 배치하고 싶은 아이템 클릭 함수 */
+  const handlePlaceItem = (itemId: number) => {
+    toggleItemModal() //모달 닫기
+
+    //아이템 리스트에서 클릭한 아이템 찾기
+    const selectedItem = itemList.find((item) => item.id === itemId)
+    if (!selectedItem) return //아이템이 없으면 종료
+
+    // 아이템 count -1로 업데이트
+    setItemList(
+      (prev) =>
+        prev
+          .map((item) => (item.id === itemId ? { ...item, count: item.count - 1 } : item))
+          .filter((item) => item.count > 0), // count가 0 이하인 아이템 제거
+    )
+
+    // 아이템 초기 위치 (컨테이너 정중앙) 계산
+    //현재 이미지 크기를 임의로 설정 추후 DB에서 받아올 수도
+    const x = decorationContainerSize.width / 2 - 80 / 2
+    const y = decorationContainerSize.height / 2 - 80 / 2
+
+    //배치중인 아이템 리스트에 추가
+    setPlacedItemList((prev) => [
+      ...prev,
+      {
+        id: uuid(),
+        name: selectedItem.name,
+        img: selectedItem.img,
+        position: {
+          x: x,
+          y: y,
+        }, //초기 위치 설정 (정중앙)
+      },
+    ])
+  }
+
+  //현재 배치중인 아이템 리스트
+  //구성은 추후 백엔드 api 따라
+  const [placedItemList, setPlacedItemList] = useState<PlacedItem[]>([
+    // { id: 1, name: '아이템1', img: Lv1Img, position: { x: 95, y: -291 } },
+    // { id: 2, name: '아이템2', img: Lv2Img, position: { x: -105, y: -129 } },
+  ])
+
+  /** 아이템 드래그 종료 시 위치 업데이트 */
+  const handlePlacedItemList = (id: string, x: number, y: number) => {
+    setPlacedItemList((prev) =>
+      prev.map((item) =>
+        item.id === id
+          ? {
+              ...item,
+              position: { x, y },
+            }
+          : item,
+      ),
+    )
+  }
+
+  //드래그 중인 아이템 id
+  const [activeItemId, setActiveItemId] = useState<string | null>(null)
+  /** 아이템 드래그 시작 시 맨 위로 올리기 */
+  const handleDragStart = (id: string) => {
+    setActiveItemId(id)
+  }
+
   //레벨별 img
-  const levelImgs = [Lv1Img, Lv2Img, Lv3Img, Lv4Img]
   const CurrentLevelImg = levelImgs[currentLevel - 1]
 
   return (
@@ -55,7 +149,7 @@ function RouteComponent() {
           className="relative mt-[23px] mb-6 flex flex-col items-center justify-end bg-gradient-to-b from-[#DCFCE7] to-[#E0F2FE]"
         >
           {/* 메뉴 아이콘 영역 */}
-          <div className="absolute top-3.5 right-[13px] flex flex-col gap-2.5">
+          <div className="absolute top-3.5 right-[13px] z-40 flex flex-col gap-2.5">
             {icons.map(({ id, Component, onClick }) => (
               <div
                 key={id}
@@ -67,8 +161,26 @@ function RouteComponent() {
             ))}
           </div>
 
-          {/* 나의 레벨 이미지 영역 */}
-          {CurrentLevelImg && <CurrentLevelImg />}
+          {/* 꾸미기 아이템 영역 */}
+          <div ref={decorationContainerRef} className="relative h-full w-full max-w-full">
+            {/* 나의 레벨 이미지 영역 */}
+            <div className="absolute bottom-0 left-1/2 z-50 -translate-x-1/2">
+              {CurrentLevelImg && <CurrentLevelImg />}
+            </div>
+
+            {/* 배치중인 아이템들 */}
+            {placedItemList.map(({ id, img: Img, position }, index) => (
+              <DecorationItem
+                handlePlacedItemList={(x, y) => handlePlacedItemList(id, x, y)}
+                key={`${id}-${position.x}-${position.y}`}
+                Img={Img}
+                position={position}
+                index={index}
+                handleDragStart={() => handleDragStart(id)}
+                isActive={activeItemId === id}
+              />
+            ))}
+          </div>
 
           {/* 현재 내 진행상황 영역 level, point, 목표*/}
           <DashboardStatus
@@ -78,7 +190,13 @@ function RouteComponent() {
           />
 
           {/* 나의 아이템 모달 */}
-          {isItemModal && <MyItemModal toggleItemModal={toggleItemModal} />}
+          {isItemModal && (
+            <MyItemModal
+              handlePlaceItem={handlePlaceItem}
+              itemList={itemList}
+              toggleItemModal={toggleItemModal}
+            />
+          )}
         </PageLayOut.BodySection>
       </PageLayOut.ScrollableContent>
       <PageLayOut.FooterSection>
