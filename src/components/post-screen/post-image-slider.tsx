6068f9d9e7610c2c927c69a7740ed6402d938c99
beomjo <dongjun1315@naver.com>
2025-11-05 17:14:43 +0900

    feat(image-slider): 이미지 슬라이더 초기 구현

---
+++ src/components/post-screen/post-image-slider.tsx
@@ -0,0 +1,22 @@
+// import { useState } from "react"
+
+// const PostImageSlider = (imageUrls: string[]) => {
+//   const [currentImage, setCurrentImage] = useState<String | undefined>(mockPostImage[0]?.url)
+
+//   return (
+//     <div className="w-full">
+//       {imageUrls.map((url) => (
+//         <img src={url}/>
+//       ))}
+//     </div>
+//   )
+// }
+
+// const mockPostImage = [
+//   {url: '/3.png'},
+//   {url: '/3.png'},
+//   {url: '/3.png'},
+//   {url: '/3.png'},
+// ]
+
+// export default PostImageSlider
