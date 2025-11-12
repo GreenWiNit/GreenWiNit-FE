bdd012956208450267e8d338f0ca473ab391ce3a
beomjo <dongjun1315@naver.com>
2025-11-12 16:00:30 +0900

    feat(top3-container): 상위 3명 렌더링 컴포넌트 구현

---
+++ src/components/ranking-screen/top3-container.tsx
@@ -0,0 +1,46 @@
+import ProfileImage from '../common/profile-image'
+import { cn } from '@/lib/utils'
+import { User } from '@/types/ranking'
+
+interface Top3ContainerProps {
+  userRank: User[]
+}
+
+const Top3Container = ({ userRank }: Top3ContainerProps) => {
+  const rankerIndex = [userRank[1], userRank[0], userRank[2]]
+
+  return (
+    <div className="border-ring mx-4 flex flex-row border-b-2 p-2 pb-4">
+      {rankerIndex.map((user, index) => {
+        const realRank = index === 1 ? 1 : index === 0 ? 2 : 3
+
+        return (
+          <div
+            key={index}
+            className={cn(
+              'mx-2 flex flex-col items-center gap-4',
+              realRank === 1 ? 'mt-2' : 'mt-4',
+            )}
+          >
+            <ProfileImage
+              imageUrl={user?.profile === undefined ? '/profile.png' : user.profile}
+              size={60}
+            />
+            <div className="bg-mountain_meadow-100 h-40 w-20 items-center rounded-md px-2 py-4">
+              <div className="mb-4 text-3xl">
+                {realRank === 1 && <span>🥇</span>}
+                {realRank === 2 && <span>🥈</span>}
+                {realRank === 3 && <span>🥉</span>}
+              </div>
+              <p className="font-bold whitespace-nowrap">{user?.name}</p>
+              <p className="text-ring text-sm whitespace-nowrap">인증{user?.certificates} 개</p>
+              <p className="text-mountain_meadow-500 mt-2 text-sm font-bold">{user?.point}P</p>
+            </div>
+          </div>
+        )
+      })}
+    </div>
+  )
+}
+
+export default Top3Container
--- src/store/rank-store.ts
+++ src/store/rank-store.ts
@@ -7,13 +7,13 @@ interface RankStore {
 
 export const useRankStore = create<RankStore>(() => ({
   users: [
-    { name: '사용자1', profile: '', certificates: 20, point: 2640, isMe: false },
-    { name: '사용자2', profile: '', certificates: 26, point: 2920, isMe: false },
-    { name: '사용자3', profile: '', certificates: 18, point: 2840, isMe: false },
-    { name: '사용자4', profile: '', certificates: 22, point: 2500, isMe: false },
-    { name: '사용자5', profile: '', certificates: 15, point: 2485, isMe: false },
-    { name: '사용자6', profile: '', certificates: 18, point: 3700, isMe: false },
-    { name: '사용자7', profile: '', certificates: 24, point: 2900, isMe: false },
-    { name: '사용자8', profile: '', certificates: 28, point: 2925, isMe: true },
+    { name: '사용자1', profile: '/img/profile.png', certificates: 20, point: 2640, isMe: false },
+    { name: '사용자2', profile: '/img/profile.png', certificates: 26, point: 2920, isMe: false },
+    { name: '사용자3', profile: '/img/profile.png', certificates: 18, point: 2840, isMe: false },
+    { name: '사용자4', profile: '/img/profile.png', certificates: 22, point: 2500, isMe: false },
+    { name: '사용자5', profile: '/img/profile.png', certificates: 15, point: 2485, isMe: false },
+    { name: '사용자6', profile: '/img/profile.png', certificates: 18, point: 3700, isMe: false },
+    { name: '사용자7', profile: '/img/profile.png', certificates: 24, point: 2900, isMe: false },
+    { name: '사용자8', profile: '/img/profile.png', certificates: 28, point: 2925, isMe: true },
   ],
 }))
