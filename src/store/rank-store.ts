c1376e45b284e0d5cef294932ec42e331c71cff2
beomjo <dongjun1315@naver.com>
2025-11-12 15:24:24 +0900

    fix(store): 목 데이터만 남겨둠

--- src/store/rank-store.ts
+++ src/store/rank-store.ts
@@ -3,13 +3,9 @@ import { create } from 'zustand'
 
 interface RankStore {
   users: User[]
-  sortedUsers: User[]
-  updateSortedUsers: () => void
-  getMyRank: () => User | undefined
-  getSliceRank: (number: number) => User[]
 }
 
-export const useRankStore = create<RankStore>((set, get) => ({
+export const useRankStore = create<RankStore>(() => ({
   users: [
     { name: '사용자1', profile: '', certificates: 20, point: 2640, isMe: false },
     { name: '사용자2', profile: '', certificates: 26, point: 2920, isMe: false },
@@ -20,23 +16,4 @@ export const useRankStore = create<RankStore>((set, get) => ({
     { name: '사용자7', profile: '', certificates: 24, point: 2900, isMe: false },
     { name: '사용자8', profile: '', certificates: 28, point: 2925, isMe: true },
   ],
-
-  sortedUsers: [],
-
-  updateSortedUsers: () => {
-    const sorted = [...get().users].sort((a, b) => {
-      if (b.point !== a.point) return b.point - a.point
-      return a.certificates - b.certificates
-    })
-    set({ sortedUsers: sorted })
-  },
-
-  getMyRank: () => get().users.find((user) => user.isMe),
-
-  getSliceRank: (number) => {
-    return get().sortedUsers.slice(0, number)
-  },
 }))
-
-// 정렬된 상태로 넘겨줌
-useRankStore.getState().updateSortedUsers()
